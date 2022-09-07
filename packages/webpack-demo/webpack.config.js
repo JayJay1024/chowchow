ee4987090e231caa6ca555911d53124855d09705
JayJay1024 <lygm1062@gmail.com>
2022-09-07 14:53:50 +0800

    Add webpack-demo project

---
+++ packages/webpack-demo/webpack.config.js
@@ -0,0 +1,28 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = {
+  mode: "production",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
+
+export default config;
