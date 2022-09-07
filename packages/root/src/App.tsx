d0c317b62a72ad02e4c6ecd4a9bfc1031df9a3a4
JayJay1024 <lygm1062@gmail.com>
2022-09-07 12:29:53 +0800

    Optimize page style

--- packages/root/src/App.tsx
+++ packages/root/src/App.tsx
@@ -1,15 +1,17 @@
 function App() {
   return (
-    <div className="flex justify-center items-center">
-      <a target="_self" href="/">
-        Home
-      </a>
-      <a target="_self" href="./block-sync">
-        Block Sync
-      </a>
-      <a target="_self" href="./tailwind-demo">
-        Tailwind Demo
-      </a>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="flex flex-col space-y-2">
+        <a target="_self" href="/">
+          Home
+        </a>
+        <a target="_self" href="./block-sync">
+          Block Sync
+        </a>
+        <a target="_self" href="./tailwind-demo">
+          Tailwind Demo
+        </a>
+      </div>
     </div>
   );
 }
