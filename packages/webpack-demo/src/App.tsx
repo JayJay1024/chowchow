83835ac431c5ad425c48a976a174a52eee9c2de7
Jay <lygm1062@gmail.com>
2022-09-09 10:34:08 +0800

    Webpack dev (#6)
    * Webpack some loaders config
    
    * Multi entry and devtool and html plugin
    
    * Dev serve
    
    * Custom template html file

---
+++ packages/webpack-demo/src/App.tsx
@@ -0,0 +1,10 @@
+import printMe from "./print";
+
+const App = () => (
+  <>
+    <span>App</span>
+    <button onClick={printMe}>Click me and check the console!</button>
+  </>
+);
+
+export default App;
--- packages/webpack-demo/webpack.config.js
+++ packages/webpack-demo/webpack.config.js
@@ -1,14 +1,38 @@
 import path from "path";
 import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const config = {
-  mode: "production",
-  entry: "./src/index.ts",
+  mode: "development",
+  entry: {
+    main: "./src/index.tsx",
+    // print: "./src/print.ts",
+  },
   module: {
     rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
+      },
       {
         test: /\.tsx?$/,
         use: "ts-loader",
@@ -19,9 +43,23 @@ const config = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
   output: {
-    filename: "bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
 };
 
